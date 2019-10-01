9cd6151ec86944439ad614bd160b8a0b7133a86a
Priyansh <priyansh7299@gmail.com>
2019-10-01 19:34:53 +0530

    HTTP request and parsing from JSON

--- app/src/main/java/com/androdocs/weatherapp/MainActivity.java
+++ app/src/main/java/com/androdocs/weatherapp/MainActivity.java
@@ -1,9 +1,14 @@
 package com.androdocs.weatherapp;
 
 //import android.os.AsyncTask;
+import android.location.Location;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import com.androdocs.httprequest.HttpRequest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -24,16 +29,17 @@ import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
-    String CITY = "dhaka,bd";
 
-    TextView addressTxt, updated_atTxt, statusTxt, tempTxt, temp_minTxt, temp_maxTxt, sunriseTxt,
-            sunsetTxt, windTxt, pressureTxt, humidityTxt, dummy;
+
+    TextView updated_atTxt, statusTxt, tempTxt, temp_minTxt, temp_maxTxt, sunriseTxt,
+            sunsetTxt, windTxt, pressureTxt, humidityTxt;
+    EditText address;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        addressTxt = findViewById(R.id.address);
+
         updated_atTxt = findViewById(R.id.updated_at);
         statusTxt = findViewById(R.id.status);
         tempTxt = findViewById(R.id.temp);
@@ -45,19 +51,69 @@ public class MainActivity extends AppCompatActivity {
         pressureTxt = findViewById(R.id.pressure);
         humidityTxt = findViewById(R.id.humidity);
 
-        dummy = findViewById(R.id.dummy);
-
-        String data="";
-        data = ( (new WeatherHttpClient()).getWeatherData(CITY));
-
-        if (data != "") {
-            dummy.setText(data);
-        }
-
+        new WeatherTask().execute();
     }
 
+    class WeatherTask extends AsyncTask<String, Void, String> {
+        String CITY = "Pune,IN";
+        @Override
+        protected String doInBackground(String... args) {
+            String data;
+            data = ((new WeatherHttpClient()).getWeatherData(CITY));
+            return data;
+        }
+
+        @Override
+        protected void onPostExecute(String data) {
+            super.onPostExecute(data);
+
+            try {
+                JSONObject jsonObj = new JSONObject(data);
+                JSONObject main = jsonObj.getJSONObject("main");
+                JSONObject sys = jsonObj.getJSONObject("sys");
+                JSONObject wind = jsonObj.getJSONObject("wind");
+                JSONObject weather = jsonObj.getJSONArray("weather").getJSONObject(0);
+
+                Long updatedAt = jsonObj.getLong("dt");
+                String updatedAtText = "Updated at: " + new SimpleDateFormat("dd/MM/yyyy hh:mm a", Locale.ENGLISH).format(new Date(updatedAt * 1000));
+                String temp = main.getString("temp") + "°C";
+                String tempMin = "Min Temp: " + main.getString("temp_min") + "°C";
+                String tempMax = "Max Temp: " + main.getString("temp_max") + "°C";
+                String pressure = main.getString("pressure");
+                String humidity = main.getString("humidity");
+
+                Long sunrise = sys.getLong("sunrise");
+                Long sunset = sys.getLong("sunset");
+                String windSpeed = wind.getString("speed");
+                String weatherDescription = weather.getString("description");
+
+                String address = jsonObj.getString("name") + ", " + sys.getString("country");
 
 
+                /* Populating extracted data into our views */
+
+                updated_atTxt.setText(updatedAtText);
+                statusTxt.setText(weatherDescription.toUpperCase());
+                tempTxt.setText(temp);
+                temp_minTxt.setText(tempMin);
+                temp_maxTxt.setText(tempMax);
+                sunriseTxt.setText(new SimpleDateFormat("hh:mm a", Locale.ENGLISH).format(new Date(sunrise * 1000)));
+                sunsetTxt.setText(new SimpleDateFormat("hh:mm a", Locale.ENGLISH).format(new Date(sunset * 1000)));
+                windTxt.setText(windSpeed);
+                pressureTxt.setText(pressure);
+                humidityTxt.setText(humidity);
+
+                /* Views populated, Hiding the loader, Showing the main design */
+                /*findViewById(R.id.loader).setVisibility(View.GONE);
+                findViewById(R.id.mainContainer).setVisibility(View.VISIBLE);*/
+
+
+            } catch (JSONException e) {
+                //findViewById(R.id.loader).setVisibility(View.GONE);
+                //findViewById(R.id.errorText).setVisibility(View.VISIBLE);
+            }
+        }
+    }
 
     /*String CITY = "dhaka,bd";
     String API = "8118ed6ee68db2debfaaa5a44c832918";
@@ -92,68 +148,34 @@ class WeatherHttpClient {
     private static String APPID = "157771a599a39dabe7b641652b7f7de3";
     public String getWeatherData(String location) {
         HttpURLConnection con = null ;
-        InputStream is = null;
+        InputStreamReader is = null;
 
         try {
-            con = (HttpURLConnection) ( new URL(BASE_URL + location + "&APPID=" + APPID)).openConnection();
+            URL url = new URL(BASE_URL + location + "&units=metric&appid=" + APPID);
+            con = (HttpURLConnection) url.openConnection();
             con.setRequestMethod("GET");
-            con.setDoInput(true);
-            con.setDoOutput(true);
             con.connect();
 
             // Let's read the response
-            StringBuffer buffer = new StringBuffer();
-            is = con.getInputStream();
-            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            is = new InputStreamReader(con.getInputStream());
+            BufferedReader reader = new BufferedReader(is);
+            StringBuilder strBuilder = new StringBuilder();
             String line = null;
-            while ( (line = br.readLine()) != null )
-                buffer.append(line + "rn");
+            while ( (line = reader.readLine()) != null )
+                strBuilder.append(line);
 
             is.close();
             con.disconnect();
-            return buffer.toString();
+            return strBuilder.toString();
         }
-        catch(Throwable t) {
-            t.printStackTrace();
-        }
-        finally {
-            try { is.close(); } catch(Throwable t) {}
-            try { con.disconnect(); } catch(Throwable t) {}
-        }
-
-        return null;
-
-    }
-    public byte[] getImage(String code) {
-        HttpURLConnection con = null ;
-        InputStream is = null;
-        try {
-            con = (HttpURLConnection) ( new URL(IMG_URL + code)).openConnection();
-            con.setRequestMethod("GET");
-            con.setDoInput(true);
-            con.setDoOutput(true);
-            con.connect();
-
-            // Let's read the response
-            is = con.getInputStream();
-            byte[] buffer = new byte[1024];
-            ByteArrayOutputStream baos = new ByteArrayOutputStream();
-
-            while ( is.read(buffer) != -1)
-                baos.write(buffer);
-
-            return baos.toByteArray();
-        }
-        catch(Throwable t) {
-            t.printStackTrace();
+        catch(Exception e) {
+            e.printStackTrace();
         }
         finally {
-            try { is.close(); } catch(Throwable t) {}
-            try { con.disconnect(); } catch(Throwable t) {}
+            try { is.close(); } catch(Exception e) {}
+            try { con.disconnect(); } catch(Exception e) {}
         }
-
-        return null;
-
+        return "";
     }
 }
 
